0e351ce9f10ef970e251c2f2687e2db197cfea58
bdarab <bdarabzand@gmail.com>
2021-11-28 21:40:30 +0100

    Create Profile Component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -37,7 +38,13 @@ const App = () => {
             <Routes>
               <Route exact path="/register" element={<Register />} />
               <Route exact path="/login" element={<Login />} />
-              <Route path='/dashboard' element={<PrivateRoute component={Dashboard} />}
+              <Route
+                path="/dashboard"
+                element={<PrivateRoute component={Dashboard} />}
+              />
+              <Route
+                path="/create-profile"
+                element={<PrivateRoute component={CreateProfile} />}
               />
             </Routes>
           </section>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -19,9 +19,9 @@ const Dashboard = ({
     <Spinner />
   ) : (
     <Fragment>
-      <h1 class="large text-primary">Dashboard</h1>
-      <p class="lead">
-        <i class="fas fa-user"></i> Welcome {user && user.name}
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome {user && user.name}
       </p>
       {profile !== null ? (
         <Fragment>has</Fragment>
