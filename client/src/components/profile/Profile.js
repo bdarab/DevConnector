ed7003d070fa43394c5edcc3cb2e0babbfce51a7
bdarab <bdarabzand@gmail.com>
2021-12-22 20:10:14 +0100

    Add Profile Experience & Education Display

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 
 const Profile = ({
   getProfileById,
@@ -50,6 +51,22 @@ const Profile = ({
                 </Fragment>
               ) : (
                 <h4>No Experience Credentials</h4>
+              )}
+            </div>
+
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education Credentials</h4>
               )}
             </div>
           </div>
