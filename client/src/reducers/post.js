88d784fcdab242c3839cdfe665ff481f9f3e8640
bdarab <bdarabzand@gmail.com>
2021-12-29 13:17:17 +0100

    Push with TypeError: setText is not a function Error

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,8 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from '../actions/types';
 
 const initialState = {
@@ -22,6 +23,18 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false
       };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        loading: false
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== payload),
+        loading: false
+      };
     case POST_ERROR:
       return {
         ...state,
@@ -32,14 +45,8 @@ export default function (state = initialState, action) {
       return {
         ...state,
         posts: state.posts.map(post =>
-          post._id === payload.id ? { ...post, likes: payload } : post
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
-        loading: false
-      };
-    case DELETE_POST:
-      return {
-        ...state,
-        posts: state.posts.filter(post => post._id !== payload),
         loading: false
       };
     default:
